e45c7cb8c70498c732d3e5649e4e288d932e6acc
Eric Youmans <ery@utexas.edu>
2025-08-28 19:58:34 -0400

    Fixed github pages config stuff

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/3zk9.io/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 })
