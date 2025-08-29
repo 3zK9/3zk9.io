58f41a27898222af9610931847e47514d62f39bf
Eric Youmans <ery@utexas.edu>
2025-08-28 21:19:03 -0400

    Fixed configs to use custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/3zk9.io/",
+  base: "/",
   build: {
     outDir: "docs",
     emptyOutDir: true,
